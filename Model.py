63d27948347f1c4770a0520bd450443cc18e3780
hgs1217 <mf20080540@126.com>
2015-12-10 10:09:32 +0800

    1

--- Model.py
+++ Model.py
@@ -6,13 +6,13 @@ All the function this file should realize is word-segmentation
 
 class Segmentation:
     def __init__(self):
-        self.lex = Lexicon("lexicon.txt")
-
+        #self.lex = Lexicon("lexicon.txt")
+        pass
     def sentence_segment(self, raw: str) -> str:
-        return "%s(已分句)" % raw
+        return "%s(宸插垎鍙?" % raw
 
     def word_segment(self, raw: str) -> str:
-        return "%s(已分词)" % raw
+        return "%s(宸插垎璇?" % raw
 
 
 class Lexicon:
@@ -20,7 +20,7 @@ class Lexicon:
         self.lex = []
 
         try:
-            src = open(filename, "r").read()
+            src = open(filename, "r", encoding = "utf-16").read()
         except FileNotFoundError:
             src = ""
 
