c52f1ea76b714cd404b3d3eb1b343f80543525ca
GigaFlower <a_gigantic_flower@yeah.net>
2015-12-23 15:02:49 +0800

    Modify test output
    Beatify the output format

--- kernel/test.py
+++ kernel/test.py
@@ -65,11 +65,11 @@ def test(start=0, amount=20):
     Do test according to test.txt and answer.txt
     :param amount:denote how many lines of tests you'd like to do,if -1,do every test.
     """
+    t0 = time.time()
+
     seg = kernel.Segmentation()
 
-    output = open("test_result", 'w')
-    tmp = ""
-
+    output = open("test_result.txt", 'w')
     src_file = open('test.txt', 'r', encoding='utf-16')
     ans_file = open('answer.txt', 'r', encoding='utf-16')
 
@@ -95,11 +95,11 @@ def test(start=0, amount=20):
         c += dc
         e += de
         if de == 0:
-            tmp = 'Sentence #%d passed!' % (test_cnt+start)
+            tmp = '\nSentence #%d passed!\n' % (test_cnt+start)
             output.write(tmp)
             print(tmp)
         else:
-            tmp = 'Sentence #%d failed!\n' % (test_cnt+start)
+            tmp = '\nSentence #%d failed!\n' % (test_cnt+start)
             tmp += 'Correct answer:\n'
             tmp += ans
             tmp += '\nYour answer:\n'
@@ -108,15 +108,18 @@ def test(start=0, amount=20):
             output.write(tmp)
             print(tmp)
 
+    t = time.time() - t0
+
     if amount == -1:
         amount = test_cnt - 1
 
     tmp = '\n\n'
     tmp += '=' * 40
     tmp += '\nAll %d tests done.\n' % amount
+    tmp += 'Time consumed: %.3fs\n' % t
     tmp += 'In all %d words in correct answer\n' % N
     tmp += '%d words have been covered\n' % c
-    tmp += 'While %d words are split wrong\n' % e
+    tmp += 'While %d words in your result are split wrong\n' % e
 
     r = c / N
     p = c / (c + e)
